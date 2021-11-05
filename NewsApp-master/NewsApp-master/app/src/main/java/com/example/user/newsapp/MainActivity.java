dfcb52adb119c5b2bbd0ec274ebf28a8a647e2f1
Kenechukwu Jane Iloegbunam <kenejane@gmail.com>
2021-11-05 12:27:08 +0100

    Add files via upload

---
+++ NewsApp-master/NewsApp-master/app/src/main/java/com/example/user/newsapp/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.user.newsapp;
+
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.Loader;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LoaderCallbacks<List<News>> {
+
+    /**
+     * URL for earthquake data from the USGS dataset
+     */
+    private static final String NEWS_REQUEST_URL =
+            "http://content.guardianapis.com/search?q=debate&tag=politics/politics&api-key=test&show-fields=byline";
+
+
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    /**
+     * Constant value for news loader ID. We can choose any integer.
+     * This really only comes into play if you're using multiple loaders.
+     */
+    private static final int NEWS_LOADER_ID = 1;
+
+    /**
+     * Adapter for the list of news
+     */
+    private NewsAdapter mAdapter;
+    private ProgressDialog pd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView newsListView = (ListView) findViewById(R.id.list);
+
+        // Create a new adapter that takes the list of news as input
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        newsListView.setAdapter(mAdapter);
+
+        // Set an item click listener on the ListView, which sends an intent to a web browser
+        // to open a website with more information about the selected earthquake.
+        newsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the current news that was clicked on
+                News currentNews = mAdapter.getItem(position);
+
+                // Convert the String URL into a URI object (to pass into the Intent constructor)
+                Uri newsUri = Uri.parse(currentNews.getUrl());
+
+                // Create a new intent to view the news URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
+                if (websiteIntent.resolveActivity(getPackageManager()) != null) {
+
+                    // Send the intent to launch a new activity
+                    startActivity(websiteIntent);
+                }
+            }});
+
+        // Get a reference to the LoaderManager, in order to interact with loaders.
+        LoaderManager loaderManager = getLoaderManager();
+
+        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+        // because this activity implements the LoaderCallbacks interface).
+        loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+
+
+    }
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
+        // Create a new loader for the given URL
+        pd = new ProgressDialog(MainActivity.this);
+        pd.setTitle("Processing...");
+        pd.setMessage("Please Wait.");
+        pd.setCancelable(false);
+        pd.setIndeterminate(true);
+        pd.show();
+        return new NewsLoader(this, NEWS_REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> news) {
+
+        //stop the progres dialog
+        pd.dismiss();
+        // Clear the adapter of previous news data
+        mAdapter.clear();
+        // If there is a valid list of {@link news, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+
+        if (news != null && !news.isEmpty()) {
+            mAdapter.addAll(news);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
+    }
+}
+
+
